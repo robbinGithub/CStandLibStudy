1f263dee791db465025295ec736aa982f7a4f7b0
robbin <robbin2117@163.com>
2017-08-29 23:58:36 +0800

    Update

--- CStandLibStudy/src/cdatetime.c
+++ CStandLibStudy/src/cdatetime.c
@@ -108,8 +108,59 @@ void test_formt_byself(){
 	char str[80];
 	t = time(NULL);
 	ptr=localtime(&t);
-	strftime(str, 100,"It is now %I %p",ptr);
+	strftime(str, 100,"It is now %I %p\n",ptr);
 	printf(str);
+
+	struct tm *newtime;
+	char tmpbuf[128];
+	t=time( NULL );
+	newtime=localtime(&t);
+	strftime( tmpbuf, 128, "Today is %A, day %d of %B in the year %Y.\n", newtime);
+	// Today is Tuesday, day 29 of August in the year 2017.
+	printf(tmpbuf);
+}
+
+/**
+ * 4.5 计算持续的时间长度
+ *
+ * 有时候在实际应用中要计算一个事件持续的时间长度，比如计算打字速度。在第1节计时部分中，我已经用clock函数举了一个例子。Clock()函数可以精确到毫秒级。
+ * 同时，我们也可以使用difftime()函数，但它只能精确到秒。该函数的定义如下：
+
+   double difftime(time_t time1, time_t time0);
+ *
+ */
+
+void time_compute_test(){
+	time_t start,end;
+	start = time(NULL);
+	system("pause");
+	end = time(NULL);
+	printf("The pause used %f seconds.\n",difftime(end,start));//<-
+	system("pause");
+}
+
+/**
+ * 4.6 分解时间转化为日历时间
+ *
+ * 这里说的分解时间就是以年、月、日、时、分、秒等分量保存的时间结构，在C/C++中是tm结构。我们可以使用mktime（）函数将用tm结构表示的时间转化为日历时间。其函数原型如下：
+
+   time_t mktime(struct tm * timeptr);
+
+       其返回值就是转化后的日历时间。这样我们就可以先制定一个分解时间，然后对这个时间进行操作了，下面的例子可以计算出1997年7月1日是星期几：
+ */
+void time_parse_test(){
+	struct tm t;
+	time_t t_of_day;
+	t.tm_year=1997-1900;
+	t.tm_mon=6;
+	t.tm_mday=1;
+	t.tm_hour=0;
+	t.tm_min=0;
+	t.tm_sec=1;
+	t.tm_isdst=0;
+	t_of_day=mktime(&t);
+	printf(ctime(&t_of_day));
+	// Tue Jul 01 00:00:01 1997
 }
 
 int main(void) {
@@ -118,7 +169,9 @@ int main(void) {
 //	time_test();
 //	gmtime_test();
 //	time_format();
-	test_formt_byself();
+//	test_formt_byself();
+//	time_compute_test();
+	time_parse_test();
 	return EXIT_SUCCESS;
 }
 
